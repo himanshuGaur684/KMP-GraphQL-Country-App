a8aedec76e5f288c189786fcc1234976d1812fe9
Himanshu <himanshugaur684@gmail.com>
2025-02-13 22:49:00 +0530

    Initial Project.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinCocoapods)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.apollo)
+    alias(libs.plugins.kotlinx.ksp)
+    alias(libs.plugins.observable.native.coroutines)
 }
 
 kotlin {
@@ -31,10 +34,37 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
+        all {
+            languageSettings.optIn("kotlinx.cinterop.ExperimentalForeignApi")
+        }
+        androidMain.dependencies {
+            implementation(libs.koin.androidx.compose)
+            implementation(libs.koin.android)
+        }
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation(libs.kotlinx.coroutines)
+
+            // koin
+            implementation(libs.koin.core)
+
+            // lifecycle
+            implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.lifecycle.viewmodel)
+
+            // Paging
+            implementation(libs.paging.common)
+
+            // KMP Observable ViewModel
+            api(libs.kmp.observableviewmodel.core)
+
+            // apollo
+            api(libs.apollo.runtime)
+            implementation(libs.apollo.normalized.cache)
+            implementation(libs.apollo.normalized.cache.sqlite)
+        }
+        iosMain.dependencies {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -52,4 +82,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
 }
